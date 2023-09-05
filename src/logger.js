17e9460e41c196997945576aa24d0fe1a72be015
Aditya Rahman <arahman5395@gmail.com>
2023-09-05 16:48:05 -0400

    Initial work on express app and server

--- src/logger.js
+++ src/logger.js
@@ -1,7 +1,7 @@
 // src/logger.js
 
 // Use `info` as our standard log level if not specified
-const options = { level: Node.process.env.LOG_LEVEL || 'info' };
+const options = { level: process.env.LOG_LEVEL || 'info' };
 
 // If we're doing `debug` logging, make the logs easier to read
 if (options.level === 'debug') {
