46713df4437710d6ee93e7be21bc57d5d4366998
Aditya Rahman <arahman5395@gmail.com>
2023-09-25 23:52:49 -0400

    Lab 3

---
+++ tests/unit/app.test.js
@@ -0,0 +1,8 @@
+/*const request = require('supertest');
+
+const app = require('../../src/app');
+
+describe('GET /v1/fragments', () => {
+  test('Resources not found', () => request(app).get('').expect(404));
+
+});*/
